4094f08acdbcbbd9bda71897e390a1c02e361c8d
FelipePininga <felipepininga@gmail.com>
2024-11-26 15:20:47 -0300

    Acessando Itens do cardápio

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from modelos.restaurantes import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_praca=Restaurante('Praça','Gourmet')
 restaurante_mexicano=Restaurante('Mexican Food','Mexicana')
 restaurante_japones=Restaurante('Japa','Japonesa')
-
-restaurante_praca.receber_avaliacao('Gui', 10)
-restaurante_praca.receber_avaliacao('Lais', 8)
-restaurante_praca.receber_avaliacao('Emy', 2)
+bebida_suco=Bebida('Suco de melancia',5.00,'Grande')
+prato_paozinho=Prato('Pãozinho',2.00,'O melhor pãozinho da cidade')
 
 def main():
-    Restaurante.listar_restaurantes()
+    pass
 
 
 if __name__=='__main__':
