2d8504bf704ebd2690849989fdf165d4dd816ecc
FelipePininga <felipepininga@gmail.com>
2024-11-26 15:54:39 -0300

    Refatorando o método

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ restaurante_mexicano=Restaurante('Mexican Food','Mexicana')
 restaurante_japones=Restaurante('Japa','Japonesa')
 bebida_suco=Bebida('Suco de melancia',5.00,'Grande')
 prato_paozinho=Prato('Pãozinho',2.00,'O melhor pãozinho da cidade')
-restaurante_praca.adicionar_bebida_no_cardapio(bebida_suco)
-restaurante_praca.adicionar_prato_no_cardapio(prato_paozinho)
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
 def main():
     pass
